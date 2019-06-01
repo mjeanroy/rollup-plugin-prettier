bae5bf6c236fe71154e5198946e179eaabf9e0b0
Mickael Jeanroy <mickael.jeanroy@gmail.com>
2019-06-01 22:07:29 +0200

    chore: update eslint configuration

--- scripts/log/index.js
+++ scripts/log/index.js
@@ -1,7 +1,7 @@
 /**
  * The MIT License (MIT)
  *
- * Copyright (c) 2017-2019 Mickael Jeanroy
+ * Copyright (c) 2016-2018 Mickael Jeanroy
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -10,40 +10,44 @@
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 
-const path = require('path');
-const gulp = require('gulp');
-const eslint = require('gulp-eslint');
-const config = require('../config');
-const log = require('../log');
+'use strict';
 
-module.exports = function lint() {
-  const src = [
-    path.join(config.root, '*.js'),
-    path.join(config.src, '**', '*.js'),
-    path.join(config.test, '**', '*.js'),
-    path.join(config.scripts, '**', '*.js'),
-  ];
+const log = require('fancy-log');
+const colors = require('ansi-colors');
 
-  log.debug('Linting files: ');
+/**
+ * Log message to output using `DEBUG` level.
+ *
+ * @param {string} msg Message to log.
+ * @return {void}
+ */
+function debug(msg) {
+  log(colors.grey(msg));
+}
 
-  src.forEach((f) => {
-    log.debug(`  ${f}`);
-  });
+/**
+ * Log message to output using `ERROR` level.
+ *
+ * @param {string} msg Message to log.
+ * @return {void}
+ */
+function error(msg) {
+  log(colors.red(msg));
+}
 
-  return gulp.src(src)
-      .pipe(eslint())
-      .pipe(eslint.format())
-      .pipe(eslint.failAfterError());
+module.exports = {
+  debug,
+  error,
 };
